8d86fac61eb7b89e3a7ea74c590c4fc2643d9185
Emmanuel <ethelbertemmanuel1@gmail.com>
2025-07-19 01:01:01 +0100

    updated resend api backend for contact

---
+++ app/api/contact/route.ts
@@ -0,0 +1,93 @@
+// Import the Resend SDK
+import Cors from 'cors';
+import initMiddleware from '../init-middleware';
+
+import {Resend} from 'resend';
+const resend = new Resend('process.env.RESEND_API_KEY');
+
+const cors = initMiddleware(
+  Cors({
+    origin: ['http://localhost:3000/', 'http://pennywiselogistics.online/'],
+    methods: ['POST', 'GET', 'OPTIONS'],
+  })
+)
+
+export default async function POST(req) {
+  // await cors(req, res);
+
+  // if(req.methods !== 'POST'){
+  //   return res.status(405).json({message: 'Method not allowed'})
+  // }
+
+  const {name, email, phone, subject, message} = await req.json();
+
+  try{
+    const data = await resend.emails.send({
+      from: email,
+      to: ['contact@pennywiselogistics.online'],
+      subject: subject || 'New Contact Message',
+      html: `
+        <p><strong>Name:</strong>${name} </p>
+        <br>
+        <p><strong>Email:</strong>${email} </p>
+        <br>
+        <p><strong>Phone:</strong>${phone} </p>
+        <br>
+        <p><strong>Message:</strong>${message} </p>
+        <br>
+      `
+    })
+    console.log('Resend Response', data);
+
+    return new Response(JSON.stringify({success: true, data}), {
+      status: 200,
+      headers:{
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+    });
+  }catch(error){
+    console.error('Contact email error', error)
+    return new Response(JSON.stringify({success: false, error: 'Failed to send email'}))
+  }
+}
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers:{
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Origin-Method': 'POST, OPTIONS',
+      'Access-Control-Allow-Origin-Header': 'Content-Type',
+    },
+  })
+}
+
+// export async function POST(req) {
+//   const body = await req.json();
+
+//   const {name, email, phone, subject, message} = body;
+
+//   try{
+//     const data = await resend.emails.send({
+//       from: email,
+//       to: ['contact@pennywiselogistics.online'],
+//       subject: subject,
+//       html: `
+//         <p><strong>Name:</strong>${name} </p>
+//         <br>
+//         <p><strong>Email:</strong>${email} </p>
+//         <br>
+//         <p><strong>Phone:</strong>${phone} </p>
+//         <br>
+//         <p><strong>Message:</strong>${message} </p>
+//         <br>
+//       `
+//     })
+//     console.log('Resend Response', data);
+
+//     return Response.json({ success: true, data, message: 'Email sent successfully' });
+//   }catch(error){
+//     return Response.json({success: false, error: error.message}, {status: 500})
+//   }
+// }
